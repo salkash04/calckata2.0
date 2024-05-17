425276e3db0d5eecc94b6d55c09552ad1a2c1e86
Салават <salkash04@mail.ru>
2024-05-17 21:53:54 +0400

    initial

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,11 @@ public class Main {
             }
 
             String num1 = parts[0].substring(1, parts[0].length() -1);
-            String num2 = parts[2].substring(1, parts[2].length() -1);
+            String num2 = parts[2].substring(1);
+            String num3 = parts[2];
             char operation = parts[1].charAt(0);
 
-            String result = calculate(num1, num2, operation);
+            String result = calculate(num1, num2, num3, operation);
 
             if (result.length() > 40) {
                 result = result.substring(0, 40) + "...";
@@ -29,28 +30,28 @@ public class Main {
         }
     }
 
-    public static String calculate(String num1, String num2, char operation) throws Exception {
+    public static String calculate(String num1, String num2, String num3, char operation) throws Exception {
         switch (operation) {
             case '+':
-                return num1 + num2;
+                return "\"" + num1 + num2;
             case '-':
                 if (num1.contains(num2)) {
-                    return num1.replace(num2, "");
+                    return "\"" + num1.replace(num2, "") + "\"";
                 } else {
-                    return num1;
+                    return "\"" + num1  + "\"";
                 }
             case '*':
-                int s1 = Integer.parseInt(num2);
+                int s1 = Integer.parseInt(num3);
                 if (s1 < 1 || s1 > 10) {
                     throw new Exception("Число должно быть от 1 до 10");
                 }
-                return num1.repeat(s1);
+                return "\"" + num1.repeat(s1) + "\"";
             case '/':
-                s1 = Integer.parseInt(num2);
+                s1 = Integer.parseInt(num3);
                 if (s1 < 1 || s1 > 10) {
                     throw new Exception("Число должно быть от 1 до 10");
                 }
-                return num1.substring(0, s1);
+                return "\"" + num1.substring(0, s1) + "\"";
             default:
                 throw new Exception("Неподдерживаемая операция");
         }
